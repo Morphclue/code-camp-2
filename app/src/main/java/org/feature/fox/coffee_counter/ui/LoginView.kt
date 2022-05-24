601eadbf4eb07c3437fc46d6549367e064f0bcce
Julian Holfeld <julian@uni-kassel.de>
2022-05-24 20:25:57 +0200

    feat: add showPassword

--- app/src/main/java/org/feature/fox/coffee_counter/ui/LoginView.kt
+++ app/src/main/java/org/feature/fox/coffee_counter/ui/LoginView.kt
@@ -1,19 +1,25 @@
 package org.feature.fox.coffee_counter.ui
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
-import androidx.compose.material.TextFieldDefaults
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.feature.fox.coffee_counter.R
@@ -26,6 +32,8 @@ fun LoginView() {
         Column(
             modifier = Modifier
                 .fillMaxWidth()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(12.dp)
         ) {
             LoginSignupHeader()
             IdTextField()
@@ -61,6 +69,7 @@ fun IdTextField() {
 @Composable
 fun PasswordTextField() {
     val passwordState = remember { mutableStateOf(TextFieldValue()) }
+    val showPassword = remember { mutableStateOf(false) }
     TextField(
         modifier = Modifier.fillMaxWidth(),
         value = passwordState.value,
@@ -71,6 +80,28 @@ fun PasswordTextField() {
             unfocusedIndicatorColor = Color.Transparent
         ),
         shape = RoundedCornerShape(8.dp),
-        visualTransformation = PasswordVisualTransformation()
+        visualTransformation = if (showPassword.value) {
+            VisualTransformation.None
+        } else {
+            PasswordVisualTransformation()
+        },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+        trailingIcon = {
+            if (showPassword.value) {
+                IconButton(onClick = { showPassword.value = false }) {
+                    Icon(
+                        imageVector = Icons.Filled.Visibility,
+                        contentDescription = stringResource(R.string.hide_password)
+                    )
+                }
+            } else {
+                IconButton(onClick = { showPassword.value = true }) {
+                    Icon(
+                        imageVector = Icons.Filled.VisibilityOff,
+                        contentDescription = stringResource(R.string.show_password)
+                    )
+                }
+            }
+        }
     )
 }
