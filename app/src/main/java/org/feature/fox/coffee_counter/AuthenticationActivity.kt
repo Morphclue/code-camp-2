bbf052425de7d6711c60f76d35c500c1377b50c9
Julian Holfeld <julian@uni-kassel.de>
2022-05-25 10:29:04 +0200

    feat: create AuthenticationActivity

---
+++ app/src/main/java/org/feature/fox/coffee_counter/AuthenticationActivity.kt
@@ -0,0 +1,19 @@
+package org.feature.fox.coffee_counter
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import org.feature.fox.coffee_counter.ui.LoginView
+import org.feature.fox.coffee_counter.ui.theme.CoffeeCounterTheme
+
+class AuthenticationActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CoffeeCounterTheme {
+                LoginView()
+            }
+        }
+    }
+}
+
