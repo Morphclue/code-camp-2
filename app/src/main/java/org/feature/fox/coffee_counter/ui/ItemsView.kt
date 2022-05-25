48a1ba99a7f7d295db458877f6996c1f716c8eab
Steffen <steffenkrapf@gmail.com>
2022-05-25 16:50:16 +0200

    feat: added Item List and Item Row

--- app/src/main/java/org/feature/fox/coffee_counter/ui/ItemsView.kt
+++ app/src/main/java/org/feature/fox/coffee_counter/ui/ItemsView.kt
@@ -18,3 +18,47 @@ fun ItemsView() {
         Text(text = "Items screen")
     }
 }
+
+
+
+@Composable
+fun ItemList(items: List<Item>) {
+    Column(
+        modifier = Modifier
+            .background(Color.White)
+            .verticalScroll(rememberScrollState())
+    ) {
+        items.forEach { item ->
+            ItemRow(item)
+        }
+    }
+}
+
+@Composable
+fun ItemRow(item: Item){
+    var buyItems by remember { mutableStateOf(0) }
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Column {
+            Text(item.name)
+            Text("${item.price}€")
+        }
+        Text("$buyItems/${item.amount}")
+        Button(onClick = {if (buyItems < item.amount) buyItems++},
+            modifier= Modifier.size(40.dp),
+            shape = CircleShape,
+            contentPadding = PaddingValues(0.dp),
+        ) {
+            // Adding an Icon "Add" inside the Button
+            Icon(Icons.Default.Add ,contentDescription = "content description", tint=Color.White)
+        }
+        Button(onClick = {if (buyItems > 0) buyItems-=1},
+            modifier= Modifier.size(40.dp),
+            shape = CircleShape,
+            contentPadding = PaddingValues(0.dp),
+        ) {
+            // Adding an Icon "Add" inside the Button
+            Icon(Icons.Default.Remove ,contentDescription = "content description", tint=Color.White)
+        }
+    }
+
+}
