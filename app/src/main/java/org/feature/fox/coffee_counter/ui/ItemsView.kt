68c9ac502773ccf41415c818d4b63c6d23d52afe
Steffen <steffenkrapf@gmail.com>
2022-05-26 15:44:12 +0200

    refactor: changed design of search bar to look more like the reference

--- app/src/main/java/org/feature/fox/coffee_counter/ui/ItemsView.kt
+++ app/src/main/java/org/feature/fox/coffee_counter/ui/ItemsView.kt
@@ -3,6 +3,7 @@ package org.feature.fox.coffee_counter.ui
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -15,6 +16,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -113,9 +115,10 @@ fun ItemRow(item: Item){
 fun SearchBar(
     modifier: Modifier = Modifier
 ) {
+    val idState = remember { mutableStateOf(TextFieldValue()) }
     TextField(
-        value = "",
-        onValueChange = {},
+        value = idState.value,
+        onValueChange = { idState.value = it },
         leadingIcon = {
             Icon(
                 imageVector = Icons.Default.Search,
@@ -123,13 +126,16 @@ fun SearchBar(
             )
         },
         colors = TextFieldDefaults.textFieldColors(
-            backgroundColor = MaterialTheme.colors.surface
+            backgroundColor = Color.LightGray,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent
         ),
         placeholder = {
             Text(stringResource(R.string.search_hint))
         },
         modifier = modifier
             .fillMaxWidth()
-            .heightIn(min = 56.dp)
+            .heightIn(min = 56.dp),
+        shape = RoundedCornerShape(50.dp),
     )
 }
