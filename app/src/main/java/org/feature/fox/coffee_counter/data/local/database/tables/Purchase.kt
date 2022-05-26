0da1abe67dd9f5c455d917f22081b5b64cb14cfa
Kevin Lüttge <69243143+wasanderes94@users.noreply.github.com>
2022-05-26 18:46:46 +0200

    feat: add purchase data class and corresponding user-relation

---
+++ app/src/main/java/org/feature/fox/coffee_counter/data/local/database/tables/Purchase.kt
@@ -0,0 +1,34 @@
+package org.feature.fox.coffee_counter.data.local.database.tables
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+
+@Entity(
+    tableName = "purchase",
+    foreignKeys = [ForeignKey(
+        entity = User::class,
+        parentColumns = arrayOf("id"),
+        childColumns = arrayOf("userId"),
+        onUpdate = ForeignKey.CASCADE,
+        onDelete = ForeignKey.CASCADE
+    )]
+)
+data class Purchase(
+
+    @PrimaryKey
+    val timestamp: Long,
+
+    @ColumnInfo(index = true)
+    val userId: String,
+
+    val value: Double,
+
+    val itemId: String,
+
+    val itemName: String,
+
+    val amount: Int
+
+)
