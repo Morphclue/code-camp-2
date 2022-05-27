9ea7905aab2e6ff9dc1cd38dbf7d39108f73804b
Kevin Lüttge <69243143+wasanderes94@users.noreply.github.com>
2022-05-27 12:37:28 +0200

    fix: add return type of funding-related queries

--- app/src/main/java/org/feature/fox/coffee_counter/data/local/dao/UserDao.kt
+++ app/src/main/java/org/feature/fox/coffee_counter/data/local/dao/UserDao.kt
@@ -2,8 +2,8 @@ package org.feature.fox.coffee_counter.data.local.dao
 
 import androidx.lifecycle.LiveData
 import androidx.room.*
-import org.feature.fox.coffee_counter.data.local.database.relations.FundingAndUser
 import org.feature.fox.coffee_counter.data.local.database.tables.Funding
+import org.feature.fox.coffee_counter.data.local.database.tables.Purchase
 import org.feature.fox.coffee_counter.data.local.database.tables.User
 
 @Dao
@@ -13,7 +13,10 @@ interface UserDao {
     suspend fun insertUser(user: User)
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertFunding(funding: List<Funding>)
+    suspend fun insertFunding(funding: Funding)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertPurchase(purchase: List<Purchase>)
 
     @Delete
     suspend fun deleteUser(user: User)
@@ -21,12 +24,15 @@ interface UserDao {
     @Delete
     suspend fun deleteFunding(funding: Funding)
 
+    @Delete
+    suspend fun deletePurchase(purchase: Purchase)
+
     @Query("SELECT * FROM users WHERE id=:id")
     suspend fun getUserById(id: String): User
 
     @Transaction
-    @Query("SELECT * FROM users WHERE id=:id")
-    suspend fun getFundingsOfUser(id: String): List<FundingAndUser>
+    @Query("SELECT * FROM funding WHERE userId=:id")
+    suspend fun getFundingListOfUser(id: String): List<Funding>
 
     @Query("SELECT CASE WHEN EXISTS (SELECT * FROM users WHERE id=:id AND password=:password) THEN CAST(1 AS BOOLEAN) ELSE CAST(0 AS BOOLEAN) END")
     suspend fun login(id: String, password: String): Boolean
