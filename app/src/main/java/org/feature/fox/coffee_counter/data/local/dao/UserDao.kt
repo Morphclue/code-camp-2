1e35446867f9b851ed92edf045b480026dc0a13b
Kevin Lüttge <69243143+wasanderes94@users.noreply.github.com>
2022-05-31 14:09:43 +0200

    feat: add queries for balance and purchase

--- app/src/main/java/org/feature/fox/coffee_counter/data/local/dao/UserDao.kt
+++ app/src/main/java/org/feature/fox/coffee_counter/data/local/dao/UserDao.kt
@@ -16,7 +16,7 @@ interface UserDao {
     suspend fun insertFunding(funding: Funding)
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertPurchase(purchase: List<Purchase>)
+    suspend fun insertPurchase(purchase: Purchase)
 
     @Delete
     suspend fun deleteUser(user: User)
@@ -33,6 +33,14 @@ interface UserDao {
     @Transaction
     @Query("SELECT * FROM funding WHERE userId=:id")
     suspend fun getFundingListOfUser(id: String): List<Funding>
+
+    @Transaction
+    @Query("SELECT * FROM purchase WHERE userId=:id")
+    suspend fun getPurchaseListOfUser(id: String): List<Purchase>
+
+    @Transaction
+    @Query("SELECT(SELECT SUM(funding.value) FROM funding WHERE userId=:id) + (SELECT SUM(purchase.totalValue) FROM purchase WHERE userId=:id)")
+    fun observeTotalBalanceOfUser(id: String): LiveData<Double>
 
     @Query("SELECT CASE WHEN EXISTS (SELECT * FROM users WHERE id=:id AND password=:password) THEN CAST(1 AS BOOLEAN) ELSE CAST(0 AS BOOLEAN) END")
     suspend fun login(id: String, password: String): Boolean
