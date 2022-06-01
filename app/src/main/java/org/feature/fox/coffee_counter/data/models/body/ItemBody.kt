e4dd5c312e4b9d63d0018f6cd228028a957f267a
Steffen <steffenkrapf@gmail.com>
2022-06-01 11:34:22 +0200

    feat: added ItemBody

---
+++ app/src/main/java/org/feature/fox/coffee_counter/data/models/body/ItemBody.kt
@@ -0,0 +1,20 @@
+package org.feature.fox.coffee_counter.data.models.body
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+class ItemBody(
+
+    @Json(name = "id")
+    val id: String,
+
+    @Json(name = "name")
+    val name: String,
+
+    @Json(name = "amount")
+    val amount: Int,
+
+    @Json(name = "price")
+    val price: Double,
+)
