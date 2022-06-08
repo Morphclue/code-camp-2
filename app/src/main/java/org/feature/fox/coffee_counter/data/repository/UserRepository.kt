654ac9e12dca558a66b414b800fd028ab5b1cf81
Steffen <steffenkrapf@gmail.com>
2022-06-08 11:27:00 +0200

    feat: added methods apiService methods for UserRepository

--- app/src/main/java/org/feature/fox/coffee_counter/data/repository/UserRepository.kt
+++ app/src/main/java/org/feature/fox/coffee_counter/data/repository/UserRepository.kt
@@ -5,7 +5,15 @@ import org.feature.fox.coffee_counter.data.local.dao.UserDao
 import org.feature.fox.coffee_counter.data.local.database.tables.Funding
 import org.feature.fox.coffee_counter.data.local.database.tables.Purchase
 import org.feature.fox.coffee_counter.data.local.database.tables.User
+import org.feature.fox.coffee_counter.data.models.body.LoginBody
+import org.feature.fox.coffee_counter.data.models.body.PurchaseBody
+import org.feature.fox.coffee_counter.data.models.body.UserBody
+import org.feature.fox.coffee_counter.data.models.response.LoginResponse
+import org.feature.fox.coffee_counter.data.models.response.TransactionResponse
+import org.feature.fox.coffee_counter.data.models.response.UserIdResponse
+import org.feature.fox.coffee_counter.data.models.response.UserResponse
 import org.feature.fox.coffee_counter.di.services.network.ApiService
+import retrofit2.Response
 import javax.inject.Inject
 
 class UserRepository @Inject constructor(
@@ -58,5 +66,49 @@ class UserRepository @Inject constructor(
 
     override fun observeAllUsers(): LiveData<List<User>>{
         return userDao.observeAllUsers()
+    }
+
+    override suspend fun postLogin(loginBody: LoginBody): Response<LoginResponse>{
+        return apiService.postLogin(loginBody)
+    }
+
+    override suspend fun getUsers(): Response<List<UserResponse>>{
+        return apiService.getUsers()
+    }
+
+    override suspend fun getUserById(id: String): Response<UserIdResponse>{
+        return apiService.getUserById(id)
+    }
+
+    override suspend fun updateUser(id: String, userBody: UserBody): Response<String>{
+        return apiService.updateUser(id, userBody)
+    }
+
+    override suspend fun signUp(userBody: UserBody): Response<String>{
+        return apiService.signUp(userBody)
+    }
+
+    override suspend fun deleteUser(id: String): Response<String>{
+        return apiService.deleteUser(id)
+    }
+
+    override suspend fun getTransactions(id: String): Response<List<TransactionResponse>>{
+        return apiService.getTransactions(id)
+    }
+
+    override suspend fun purchaseItem(id: String, purchaseBody: PurchaseBody): Response<String>{
+        return apiService.purchaseItem(id, purchaseBody)
+    }
+
+    override suspend fun adminSignUp(userBody: UserBody): Response<String>{
+        return apiService.adminSignUp(userBody)
+    }
+
+    override suspend fun updateAdmin(id: String, userBody: UserBody): Response<String>{
+        return apiService.updateAdmin(id, userBody)
+    }
+
+    override suspend fun addFunding(id: String, fundingBody: Double): Response<String>{
+        return apiService.addFunding(id, fundingBody)
     }
 }
