6d9603610db260570e17a1fefdd29284e46c0c0d
Julian Holfeld <julian@uni-kassel.de>
2022-06-10 16:54:16 +0200

    feat: add ModalBottomSheet

---
+++ app/src/main/java/org/feature/fox/coffee_counter/ui/EditUserView.kt
@@ -0,0 +1,11 @@
+package org.feature.fox.coffee_counter.ui
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview()
+@Composable
+fun EditUserView() {
+    Text(text = "Test")
+}
