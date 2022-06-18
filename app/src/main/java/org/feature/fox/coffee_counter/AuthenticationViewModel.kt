b4fc7dddc7b16bca77d9296705c12215c878fa69
Julian Holfeld <julian@uni-kassel.de>
2022-06-18 19:22:00 +0200

    feat: add AuthenticationViewModel

---
+++ app/src/main/java/org/feature/fox/coffee_counter/AuthenticationViewModel.kt
@@ -0,0 +1,8 @@
+package org.feature.fox.coffee_counter
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class AuthenticationViewModel @Inject constructor() : ViewModel()
