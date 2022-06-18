873ba27454b1e60f6c3fe47ce91729b5b2e5687c
Julian Holfeld <julian@uni-kassel.de>
2022-06-18 19:21:46 +0200

    feat: add @AndroidEntryPoint

--- app/src/main/java/org/feature/fox/coffee_counter/MainActivity.kt
+++ app/src/main/java/org/feature/fox/coffee_counter/MainActivity.kt
@@ -3,9 +3,11 @@ package org.feature.fox.coffee_counter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import dagger.hilt.android.AndroidEntryPoint
 import org.feature.fox.coffee_counter.ui.HomeView
 import org.feature.fox.coffee_counter.ui.theme.CoffeeCounterTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
