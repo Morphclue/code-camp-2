dd2541895274fdc7f22bd59e00496de2454cb722
Julian Holfeld <julian@uni-kassel.de>
2022-06-22 14:52:32 +0200

    feat: provide userRepository

--- app/src/main/java/org/feature/fox/coffee_counter/AuthenticationViewModel.kt
+++ app/src/main/java/org/feature/fox/coffee_counter/AuthenticationViewModel.kt
@@ -5,6 +5,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import org.feature.fox.coffee_counter.data.models.body.LoginBody
+import org.feature.fox.coffee_counter.data.repository.UserRepository
+import org.feature.fox.coffee_counter.di.module.ApiModule
 import javax.inject.Inject
 
 interface IAuthenticationViewModel {
@@ -17,7 +20,9 @@ interface IAuthenticationViewModel {
 }
 
 @HiltViewModel
-class AuthenticationViewModel @Inject constructor() : ViewModel(), IAuthenticationViewModel {
+class AuthenticationViewModel @Inject constructor(
+    private val userRepository: UserRepository,
+) : ViewModel(), IAuthenticationViewModel {
     override var name: String by mutableStateOf("")
     override var id: String by mutableStateOf("")
     override var password: String by mutableStateOf("")
---
+++ app/src/main/java/org/feature/fox/coffee_counter/di/module/DatabaseModule.kt
@@ -0,0 +1,28 @@
+package org.feature.fox.coffee_counter.di.module
+
+import android.content.Context
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import org.feature.fox.coffee_counter.data.local.UserDatabase
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Singleton
+    @Provides
+    fun providesUserDataBase(
+        @ApplicationContext context: Context,
+    ) = Room.databaseBuilder(context, UserDatabase::class.java, "users").build()
+
+    @Singleton
+    @Provides
+    fun providesUserDao(
+        database: UserDatabase,
+    ) = database.userDao()
+}
