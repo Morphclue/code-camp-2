27936c365a988665fd9e6ab6585091fc8a4fbcd8
Kevin Lüttge <69243143+wasanderes94@users.noreply.github.com>
2022-06-24 10:07:49 +0200

    feat: added event class for live-data handling

---
+++ app/src/main/java/org/feature/fox/coffee_counter/util/Event.kt
@@ -0,0 +1,17 @@
+package org.feature.fox.coffee_counter.util
+
+open class Event<out T>(private val content: T) {
+
+    var hasBeenHandled = false
+        private set // Allow external read but not write
+
+    fun getContentIfNotHandled(): T? {
+        return if (hasBeenHandled) null
+        else {
+            hasBeenHandled = true
+            content
+        }
+    }
+
+    fun peekContent(): T = content
+}
