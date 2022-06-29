9a136a674049237bf39206fecc8f247cee008b3d
Julian Holfeld <julian@uni-kassel.de>
2022-06-29 12:27:29 +0200

    feat: add AppPreference.kt

---
+++ app/src/main/java/org/feature/fox/coffee_counter/di/services/AppPreference.kt
@@ -0,0 +1,20 @@
+package org.feature.fox.coffee_counter.di.services
+
+import android.content.Context
+import androidx.preference.PreferenceManager
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class AppPreference @Inject constructor(@ApplicationContext context: Context) {
+    private val preferences = PreferenceManager.getDefaultSharedPreferences(context)
+
+    fun getTag(name: String): String {
+        return preferences.getString(name, "") ?: return ""
+    }
+
+    fun setTag(name: String, content: String) {
+        preferences.edit().putString(name, content).apply()
+    }
+}
