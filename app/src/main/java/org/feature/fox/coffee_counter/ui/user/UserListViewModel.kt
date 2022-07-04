a0be1aaef89d9b2dfcfec7c6148ae574442cce2f
Julian Holfeld <julian@uni-kassel.de>
2022-07-04 11:53:11 +0200

    feat: fetch all users

--- app/src/main/java/org/feature/fox/coffee_counter/ui/user/UserListViewModel.kt
+++ app/src/main/java/org/feature/fox/coffee_counter/ui/user/UserListViewModel.kt
@@ -1,14 +1,46 @@
 package org.feature.fox.coffee_counter.ui.user
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import org.feature.fox.coffee_counter.data.local.database.tables.User
+import org.feature.fox.coffee_counter.data.repository.UserRepository
 import javax.inject.Inject
 
-interface IUserListViewModel
+interface IUserListViewModel {
+    val userList: MutableList<User>
+}
 
 @HiltViewModel
 class UserListViewModel @Inject constructor(
+    private val userRepository: UserRepository,
+) : ViewModel(), IUserListViewModel {
+    override val userList = mutableListOf<User>()
 
-) : ViewModel(), IUserListViewModel
+    init {
+        viewModelScope.launch {
+            loadUsers()
+        }
+    }
 
-class UserListViewModelPreview : IUserListViewModel
+    private suspend fun loadUsers() {
+        val response = userRepository.getUsers()
+        if (response.data == null) {
+            return
+        }
+
+        response.data.forEach { user ->
+            val idResponse = userRepository.getUserById(user.id)
+
+            if (idResponse.data == null) {
+                return@forEach
+            }
+            // TODO
+        }
+    }
+}
+
+class UserListViewModelPreview : IUserListViewModel {
+    override val userList = mutableListOf<User>()
+}
