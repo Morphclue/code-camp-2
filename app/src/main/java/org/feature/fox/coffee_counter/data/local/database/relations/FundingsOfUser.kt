9a5782bfe6f919bcc67acf991739a09002b2126e
Kevin Lüttge <69243143+wasanderes94@users.noreply.github.com>
2022-07-26 18:57:16 +0200

    refactor: renamed relation classes

---
+++ app/src/main/java/org/feature/fox/coffee_counter/data/local/database/relations/FundingsOfUser.kt
@@ -0,0 +1,17 @@
+package org.feature.fox.coffee_counter.data.local.database.relations
+
+import androidx.room.Embedded
+import androidx.room.Relation
+import org.feature.fox.coffee_counter.data.local.database.tables.Funding
+import org.feature.fox.coffee_counter.data.local.database.tables.User
+
+data class FundingsOfUser(
+
+    @Embedded val user: User,
+
+    @Relation(
+        parentColumn = "userId",
+        entityColumn = "userId"
+    )
+    val fundingList: List<Funding>
+)
