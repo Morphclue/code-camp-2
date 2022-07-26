dec0005fc89a36c6baa55c81ed6cff9995f0cf57
Kevin Lüttge <69243143+wasanderes94@users.noreply.github.com>
2022-07-26 19:16:00 +0200

    refactor/fix: add TODOs for each unused method, fixed annotations and queries

--- app/src/main/java/org/feature/fox/coffee_counter/data/local/database/dao/ItemDao.kt
+++ app/src/main/java/org/feature/fox/coffee_counter/data/local/database/dao/ItemDao.kt
@@ -3,27 +3,33 @@ package org.feature.fox.coffee_counter.data.local.database.dao
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Update
 import org.feature.fox.coffee_counter.data.local.database.tables.Item
 
 @Dao
 interface ItemDao {
-    @Update
-    suspend fun insertItem(item: Item)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertItemDb(item: Item)
 
     @Delete
-    suspend fun deleteItem(item: Item)
+    suspend fun deleteItemDb(item: Item)
 
     @Update
-    suspend fun updateItem(item: Item)
+    suspend fun updateItemDb(item: Item)
 
+    //TODO Delete or use me
     @Query("SELECT * FROM items WHERE id=:id")
-    suspend fun getItemById(id: String): Item
+    suspend fun getItemByIdDb(id: String): Item
 
+    //TODO Delete or use me
     @Query("SELECT * FROM items")
-    fun observeAllItems(): LiveData<List<Item>>
+    fun observeAllItemsDb(): LiveData<List<Item>>
 
+    //TODO Delete or use me
     @Query("SELECT SUM (price * amount) FROM items")
-    fun observeTotalPrice(): LiveData<Double>
+    fun observeTotalPriceDb(): LiveData<Double>
 }
--- app/src/main/java/org/feature/fox/coffee_counter/data/local/database/dao/UserDao.kt
+++ app/src/main/java/org/feature/fox/coffee_counter/data/local/database/dao/UserDao.kt
@@ -3,51 +3,68 @@ package org.feature.fox.coffee_counter.data.local.database.dao
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.RewriteQueriesToDropUnusedColumns
 import androidx.room.Transaction
 import androidx.room.Update
+import org.feature.fox.coffee_counter.data.local.database.relations.FundingsOfUser
 import org.feature.fox.coffee_counter.data.local.database.tables.Funding
 import org.feature.fox.coffee_counter.data.local.database.tables.Purchase
 import org.feature.fox.coffee_counter.data.local.database.tables.User
 
 @Dao
 interface UserDao {
-    @Update
-    suspend fun insertUser(user: User)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertUserDb(user: User)
 
     @Update
-    suspend fun insertFunding(funding: Funding)
+    suspend fun updateUserDb(user: User)
 
-    @Update
-    suspend fun insertPurchase(purchase: Purchase)
-
-    @Delete
-    suspend fun deleteUser(user: User)
+    @RewriteQueriesToDropUnusedColumns
+    @Query("SELECT isAdmin FROM users WHERE userId = :userId")
+    suspend fun getAdminStateOfUserByIdDb(userId: String): Boolean
 
     @Delete
-    suspend fun deleteFunding(funding: Funding)
+    suspend fun deleteUserDb(user: User)
 
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertFundingDb(funding: Funding)
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertPurchaseDb(purchase: Purchase)
+
+
+    //TODO Delete or use me
     @Delete
-    suspend fun deletePurchase(purchase: Purchase)
+    suspend fun deleteFundingDb(funding: Funding)
 
-    @Query("SELECT * FROM users WHERE id=:id")
-    suspend fun getUserById(id: String): User
+    //TODO Delete or use me
+    @Delete
+    suspend fun deletePurchaseDb(purchase: Purchase)
 
+    //TODO Delete or use me
+    @Query("SELECT * FROM users WHERE userId=:userId")
+    suspend fun getUserByIdDb(userId: String): User
+
+    //TODO Delete or use me
     @Transaction
-    @Query("SELECT * FROM funding WHERE userId=:id")
-    suspend fun getFundingListOfUser(id: String): List<Funding>
+    @Query("SELECT * FROM users")
+    suspend fun getFundingListOfUserDb(): List<FundingsOfUser>
 
+    //TODO Delete or use me
     @Transaction
     @Query("SELECT * FROM purchase WHERE userId=:id")
-    suspend fun getPurchaseListOfUser(id: String): List<Purchase>
+    suspend fun getPurchaseListOfUserDb(id: String): List<Purchase>
 
+    //TODO Delete or use me
     @Transaction
     @Query("SELECT(SELECT SUM(funding.value) FROM funding WHERE userId=:id) + (SELECT SUM(purchase.totalValue) FROM purchase WHERE userId=:id)")
-    fun observeTotalBalanceOfUser(id: String): LiveData<Double>
+    fun observeTotalBalanceOfUserDb(id: String): LiveData<Double>
 
-    @Query("SELECT CASE WHEN EXISTS (SELECT * FROM users WHERE id=:id AND password=:password) THEN CAST(1 AS BOOLEAN) ELSE CAST(0 AS BOOLEAN) END")
-    suspend fun login(id: String, password: String): Boolean
-
+    //TODO Delete or use me
     @Query("SELECT * FROM users")
-    fun observeAllUsers(): LiveData<List<User>>
+    fun observeAllUsersDb(): LiveData<List<User>>
 }
