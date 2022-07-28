77bc9397bbfe7b35a870f606a87ff05bfda1d6eb
Kevin Lüttge <kevin.luettge@hubner-germany.com>
2022-07-28 10:03:34 +0200

    refactor: changed dateFormat for lineChart

--- app/src/main/java/org/feature/fox/coffee_counter/util/DateTimeFormatter.kt
+++ app/src/main/java/org/feature/fox/coffee_counter/util/DateTimeFormatter.kt
@@ -1,12 +1,14 @@
 package org.feature.fox.coffee_counter.util
 
 import com.github.mikephil.charting.formatter.ValueFormatter
+import org.feature.fox.coffee_counter.BuildConfig
 import java.text.SimpleDateFormat
+import java.util.*
 
 class DateTimeFormatter : ValueFormatter() {
 
     override fun getFormattedValue(value: Float): String {
-        val sdf = SimpleDateFormat("W/MM")
+        val sdf = SimpleDateFormat(BuildConfig.DATE_PATTERN, Locale.GERMAN)
         return sdf.format(value.toLong())
     }
 }
