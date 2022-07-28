ad973f7b3ad7ae03d2c8e1ad9f246dca03001474
Kevin Lüttge <69243143+wasanderes94@users.noreply.github.com>
2022-07-28 15:42:57 +0200

    fix: changed to CoroutineWorker for suspend functions & changed to PeriodicRequest

--- app/src/main/java/org/feature/fox/coffee_counter/MainActivity.kt
+++ app/src/main/java/org/feature/fox/coffee_counter/MainActivity.kt
@@ -5,12 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.work.Constraints
 import androidx.work.NetworkType
-import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkManager
 import dagger.hilt.android.AndroidEntryPoint
 import org.feature.fox.coffee_counter.ui.authentication.HomeView
 import org.feature.fox.coffee_counter.ui.theme.CoffeeCounterTheme
 import org.feature.fox.coffee_counter.util.worker.BalanceWorker
+import java.util.concurrent.TimeUnit
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -18,7 +19,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         val constraints =
             Constraints.Builder().setRequiredNetworkType(NetworkType.CONNECTED).build()
-        val request = OneTimeWorkRequestBuilder<BalanceWorker>().setConstraints(constraints).build()
+        val request = PeriodicWorkRequestBuilder<BalanceWorker>(5, TimeUnit.SECONDS).setConstraints(
+            constraints
+        ).build()
         WorkManager.getInstance(this).enqueue(request)
         setContent {
             CoffeeCounterTheme {
--- app/src/main/java/org/feature/fox/coffee_counter/util/worker/BalanceWorker.kt
+++ app/src/main/java/org/feature/fox/coffee_counter/util/worker/BalanceWorker.kt
@@ -3,19 +3,30 @@ package org.feature.fox.coffee_counter.util.worker
 import android.content.Context
 import android.util.Log
 import androidx.hilt.work.HiltWorker
-import androidx.work.Worker
+import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import org.feature.fox.coffee_counter.BuildConfig
+import org.feature.fox.coffee_counter.data.repository.UserRepository
+import org.feature.fox.coffee_counter.di.services.AppPreference
 
 @HiltWorker
 class BalanceWorker @AssistedInject constructor(
     @Assisted context: Context,
-    @Assisted workerParams: WorkerParameters
-) : Worker(context, workerParams) {
+    @Assisted workerParams: WorkerParameters,
+    private val userRepository: UserRepository,
+    private val preference: AppPreference,
+) : CoroutineWorker(context, workerParams) {
 
-    override fun doWork(): Result {
-        Log.d("WORK", "Worker running")
+    override suspend fun doWork(): Result {
+        withContext(Dispatchers.IO) {
+            val temp = userRepository.getTransactions(preference.getTag(BuildConfig.USER_ID))
+            Log.d("WORK", "Worker running")
+            Log.d("WORK", "Size of transactions is: ${temp.data?.size}")
+        }
         return Result.success()
     }
 }
